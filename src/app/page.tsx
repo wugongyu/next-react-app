e438fd18baddb07e76c10fa4ebe5f355bc6b8179
xuhongyu <1634501072@qq.com>
2023-12-14 17:31:03 +0800

    fix: router problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 const Home: React.FC<{}> = () => {
-  const { locale } = useRouter();
   return (
     <section className="flex flex-col items-center gap-y-5 mt-12 sm:mt-36">
       <Image 
@@ -18,7 +17,7 @@ const Home: React.FC<{}> = () => {
       <Search />
       <p>
         Google offered in:{' '}
-        <Link href="/" locale={locale === 'en' ? 'fr' : 'en'} className="underline text-blue-600">
+        <Link href="/" className="underline text-blue-600">
           Français
         </Link>
       </p>
